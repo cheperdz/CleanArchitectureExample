5c9406ab08a516cb06df5afc1a182627d55871b8
irony2d <irony2d@gmail.com>
2025-05-08 14:53:19 -0600

    Replace error with code in result pattern implementation.
    Refactored the Result class to use Code instead of Error for greater clarity and flexibility in handling status codes and messages. Updated WeatherRepository and GeneralError accordingly.

---
+++ Patterns/Result/GeneralError.cs
@@ -0,0 +1,11 @@
+﻿namespace Patterns.Result;
+
+public static class GeneralError
+{
+    // 400's
+    public static readonly Code BadRequest = new (400, "Input is not valid");
+    public static readonly Code NotFound = new (404, "Id not found");
+
+    // 500's
+    public static readonly Code InternalServerError = new (500, "Internal server error");
+}
